fd6dc1e1eecdd57156ec41beddc20ba2042ca281
Renato <renatoalves92@gmail.com>
2022-02-05 13:55:15 -0300

    refactor: move locally placed custom fonts to custom document

--- src/pages/newbie/nftcard.tsx
+++ src/pages/newbie/nftcard.tsx
@@ -1,18 +1,5 @@
-import Head from 'next/head'
-
-import styles from '../../styles/newbie/nftcard.module.css'
+import styles from "../../styles/newbie/nftcard.module.css";
 
 export default function NFTPage() {
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600&display=swap" rel="stylesheet" />
-      </Head>
-      <main className={styles.main}>
-        {/* <QrCode /> */}
-      </main>
-    </>
-  )
+  return <main className={styles.main}>{/* <QrCode /> */}</main>;
 }
--- src/pages/newbie/qrcode.tsx
+++ src/pages/newbie/qrcode.tsx
@@ -1,19 +1,10 @@
-import Head from 'next/head'
-
-import styles from '../../styles/newbie/qrcode.module.css'
-import QrCode from '../../modules/newbie/qrcode'
+import styles from "../../styles/newbie/qrcode.module.css";
+import QrCode from "../../modules/newbie/qrcode";
 
 export default function QRCode() {
   return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap" rel="stylesheet" />
-      </Head>
-      <main className={styles.main}>
-        <QrCode />
-      </main>
-    </>
-  )
+    <main className={styles.main}>
+      <QrCode />
+    </main>
+  );
 }
