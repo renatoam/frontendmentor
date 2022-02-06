f29fc72a00ad53f0461efe2f3dd995bd4042ee59
Renato <renatoalves92@gmail.com>
2022-02-06 12:41:54 -0300

    chore: add and configure material icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,14 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
         </Head>
         <body>
           <Main />
